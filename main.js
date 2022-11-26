5b17dcf2c4c3bfc4bacf5cabf76fad0ae467c4d6
ukadir <usman1kadir@gmail.com>
2022-11-26 15:25:39 -0800

    switched js

---
+++ main.js
@@ -0,0 +1,14 @@
+let form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      let output = document.querySelector('output');
+      let firstNum = document.querySelector('#first-num').value;
+      let secondNum = document.querySelector('#second-num').value;
+      let operator = document.querySelector('#operator').value;
+      output.innerHTML = eval(`${firstNum} ${operator} ${secondNum}`);
+    });
+
+    let errorBtns = Array.from(document.querySelectorAll('#error-btns > button'));
+
+    // Start your code here
+    // You may move this JS to another file if you wish
